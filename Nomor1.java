9acd2fa11e6c1b60fab8d52ec33879f8dd88d225
iannisafitr <annisafitria9900@gmail.com>
2024-12-12 09:24:20 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -6,7 +6,20 @@ public class Nomor1 {
         Scanner input = new Scanner(System.in);
         int N = input.nextInt();
         /* Pekerjaan anda mulai dari sini */
+        tampilkanAngkaGanjil(n);
+        
+        scanner.close();
+    }
 
+    // Menampilkan angka ganjil
+    public static void tampilkanAngkaGanjil(int n) {
+        System.out.println("Angka ganjil dari 0 hingga " + n + ":");
+        for (int i = 0; i <= n; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+    }
         /* Pekerjaan anda berakhir sini */
 
         System.out.println(Nomor1.class.getDeclaredMethods().length); 
